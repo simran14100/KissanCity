61d144eb6f3f9e531f5259b961a71014c630d90d
simran14100 <simranjha191@gmail.com>
2026-02-19 12:12:54 +0530

    f

--- vite.config.ts
+++ vite.config.ts
@@ -13,12 +13,12 @@ export default defineConfig(({ mode }) => ({
     },
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://localhost:5055",
         changeOrigin: true,
         secure: false,
       },
       "/uploads": {
-        target: "http://localhost:5000",
+        target: "http://localhost:5055",
         changeOrigin: true,
         secure: false,
       },
